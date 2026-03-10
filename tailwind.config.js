81e0fd5505abb9e4f595a172e1a51a1618429763
Satya Adriansyah <satyaadriansyah@gmail.com>
2026-03-11 00:16:39 +0700

    Create tailwind.config.js for Tailwind CSS configuration in Laravel

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+ * Tailwind CSS configuration for Laravel
+ */
+
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
